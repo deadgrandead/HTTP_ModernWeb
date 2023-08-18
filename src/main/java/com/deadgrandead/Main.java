b3f7fa1a3ec5e566ba42259cb753b02ee98a4180
Andrew Romanov <92754319+deadgrandead@users.noreply.github.com>
2023-08-18 17:11:48 +0300

    Update Main.java

--- src/main/java/com/deadgrandead/Main.java
+++ src/main/java/com/deadgrandead/Main.java
@@ -2,7 +2,10 @@ package com.deadgrandead;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server();
+        int threads = 64;
+        int port = 9999;
+
+        Server server = new Server(threads, port);
         server.start();
     }
 }
